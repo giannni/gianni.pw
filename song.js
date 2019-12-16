db8f36ed869b984a448991ab8e341af583735ebf
gianni <giannie98@gmail.com>
2019-12-16 11:30:57 -0500

    updates

---
+++ song.js
@@ -0,0 +1,17 @@
+var songs =
+[
+    { filename: "wwjd.mp3"},
+    { filename: "rockout.mp3"},
+    { filename: "motorola.mp3"},
+    { filename: "instrumental.mp3"},
+	{ filename: "prettyface.mp3"},
+    { filename: "kiss.mp3"},
+    { filename: "aboutu.mp3"},
+    { filename: "bean.mp3"},
+];
+
+var randomIndex = Math.floor(Math.random() * songs.length);
+var song = songs[randomIndex];
+document.querySelector("source").src = song.filename;
+var audio = document.currentScript.parentElement;
+audio.volume = 0.20;
