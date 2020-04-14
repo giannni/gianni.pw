8936c606c8949f4b14dee6da18a9e2f3dc75670b
gianni <giannie98@gmail.com>
2020-04-14 08:14:20 -0400

    Update song.js

--- js/song.js
+++ js/song.js
@@ -5,4 +5,4 @@ function randomNumber(min, max) {
 var randomInt = randomNumber(1,39);
 var song = randomInt.toString();
 var audio = document.querySelector("source").src = "./music/" + song + ".mp3";
-var audio = document.getElementById("music"); audio.volume = 0.1;
+audio.volume = 0.1;
