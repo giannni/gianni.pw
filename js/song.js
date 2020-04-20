fce7973f2b2d32e06d97030d9857893c0b62be25
gianni <giannie98@gmail.com>
2020-04-19 22:44:53 -0400

    Update song.js

--- js/song.js
+++ js/song.js
@@ -2,9 +2,16 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min); 
 }  
 
+function selectSong()
+{
+    var randomInt = randomNumber(1,54);
+    var song = randomInt.toString();
+    var audio = document.querySelector("source").src = "./music/" + song + ".mp3";
+}
 
-var randomInt = randomNumber(1,54);
-var song = randomInt.toString();
-var audio = document.querySelector("source").src = "./music/" + song + ".mp3";
 var music = document.getElementById("music");
 music.volume = 0.03;
+
+music.onended = function() {
+    selectSong();
+};
