ca16f7bc073374b9db30b58ef6adb8e5462243af
gianni <giannie98@gmail.com>
2020-05-02 07:52:29 -0400

    updates

--- js/rain.js
+++ js/rain.js
@@ -23,4 +23,22 @@ var makeItRain = function() {
   $('.rain.back-row').append(backDrops);
 }
 
+$('.splat-toggle.toggle').on('click', function() {
+  $('body').toggleClass('splat-toggle');
+  $('.splat-toggle.toggle').toggleClass('active');
+  makeItRain();
+});
+
+$('.back-row-toggle.toggle').on('click', function() {
+  $('body').toggleClass('back-row-toggle');
+  $('.back-row-toggle.toggle').toggleClass('active');
+  makeItRain();
+});
+
+$('.single-toggle.toggle').on('click', function() {
+  $('body').toggleClass('single-toggle');
+  $('.single-toggle.toggle').toggleClass('active');
+  makeItRain();
+});
+
 makeItRain();
