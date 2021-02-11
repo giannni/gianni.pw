8b5f2d4e56dc52fb000b203b80abde9d6d80adc0
gianni <giannie98@gmail.com>
2021-02-10 20:48:45 -0500

    update

--- js/song.js
+++ js/song.js
@@ -1,21 +1,24 @@
-function randomNumber(min, max) 
+function random_number(min, max) 
 {  
     return Math.floor(Math.random() * (max - min) + min); 
 }
 
 var music = document.getElementById("music");
 
-function selectSong()
+const audio_sources = ["bleedout.mp3","main character.mp3",'blue mondays.mp3',"hatchets.mp3","woodside.mp3","craft.mp3","sheaskedwhatmylifeislike.mp3","yyy.mp3"]
+
+function select_song()
 {
-    var randomInt = randomNumber(1,10);
-    var song = randomInt.toString();
-    var audio = document.querySelector("source").src = "./music/" + song + ".mp3";
-    music.volume = 0.03;
+    var random_int = random_number(1,8);
+    var song = audio_sources[random_int]
+    var audio = document.querySelector("source").src = "./music/" + song;
+    music.volume = 0.1;
     music.play();
 }
 
-selectSong();
+select_song();
 
-music.onended = function() {
-    selectSong();
+music.onended = function() 
+{
+    select_song();
 };
